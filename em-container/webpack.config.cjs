0c23b2fb15149e22c35024ce306cc2245cd1b6a8
ointikhab <ointikhab97@gmail.com>
2026-03-09 21:26:31 +0500

    added remote in container

---
+++ em-container/webpack.config.cjs
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist-webpack'),
+    filename: '[name].js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.vue'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      // Vue SFC
+      { test: /\.vue$/, loader: 'vue-loader' },
+
+      // TypeScript
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              appendTsSuffixTo: [/\.vue$/], // handle <script lang="ts">
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+
+      // CSS / Tailwind
+      {
+        test: /\.css$/i,
+        use: [
+          'vue-style-loader', // injects CSS into DOM
+          {
+            loader: 'css-loader', // interprets @import and url()
+            options: { importLoaders: 1 },
+          },
+          'postcss-loader', // processes Tailwind + autoprefixer
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({ template: './index.html' }),
+    new ModuleFederationPlugin({
+      name: 'app1',
+      filename: 'remoteEntry.js',
+      remotes: {
+        './detailApp': 'detailApp@http://localhost:3001/remoteEntry.js',
+      },
+      shared: {
+        vue: {
+          singleton: true,       // Only one Vue instance across MFEs
+          eager: true,           // Load immediately
+          requiredVersion: '^3.5.28',
+        },
+      },
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+  },
+};
